2625d75a98a8d246fbeb36a813f2b82b0bbc63f4
buddhacris <mitchkarmen@yahoo.com>
2018-01-23 19:28:07 -0500

    Add files via upload
    Added orbit simulation, using euler's integration and not leapfrog yet

---
+++ Leapfrog.py
@@ -0,0 +1,36 @@
+import astropy
+import numpy as np
+import matplotlib.pyplot as plt
+
+m_jup = 1.898e27 #kg
+d = 8.157e11 #m
+m_sun = 1.989e30 #kg
+G = 6.674e-11 #m^3 kg ^-1 s ^-2
+v_jup = 1.31e4 #ms^-1
+GmM = m_sun*m_jup*G
+dt = 10000
+
+#initial position of jupiter
+x = [d]
+y = [0.0]
+
+u = [0.0] #velocity in the y-direction
+v = [v_jup] #velocity in the x-direction
+
+def integrate(x, y, u, v, G, M, dt):
+	for i in range(0, 100000):
+		r = np.sqrt(x[i]**2 + y[i]**2)
+
+		ax = G*M*x[i]/(r**3)
+		ay = G*M*y[i]/(r**3)
+
+		u.append( u[i] - dt*ax )
+		v.append( v[i] - dt*ay )
+
+		x.append( x[i] + dt*u[i] )
+		y.append( y[i] + dt*v[i] )
+
+	plt.plot(x, y)
+	plt.show()
+
+integrate(x, y, u, v, G, m_sun, dt)
